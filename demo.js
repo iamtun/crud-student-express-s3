bdce367e31ca3ce197ccf29ff2382793619d7319
iamtun <letuan19431791.iuh@gmail.com>
2024-05-05 21:02:27 +0700

    add cors

--- demo.js
+++ demo.js
@@ -2,12 +2,15 @@ import express from 'express';
 import studentRouters from "./router/student.router.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import dotenv from 'dotenv'; 
+import cors from 'cors';
+
 const PORT = process.env.PORT || 3000;
 
 dotenv.config();
 await connectToMongoDB();
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 app.use ("/api/students", studentRouters);
